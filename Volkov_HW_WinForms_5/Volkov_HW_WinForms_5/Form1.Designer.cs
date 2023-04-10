1eab1b23341465aaf3fa92b347358a731bac89d0
Landic <danil.volkov.235@gmail.com>
2023-04-10 11:35:07 +0300

    Добавил дз за 10.04.2023

--- Volkov_HW_WinForms_5/Volkov_HW_WinForms_5/Form1.Designer.cs
+++ Volkov_HW_WinForms_5/Volkov_HW_WinForms_5/Form1.Designer.cs
@@ -403,7 +403,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 376);
+            this.ClientSize = new System.Drawing.Size(548, 389);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
--- Volkov_HW_WinForms_5/Volkov_HW_WinForms_5/Form1.cs
+++ Volkov_HW_WinForms_5/Volkov_HW_WinForms_5/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,12 @@ namespace Volkov_HW_WinForms_5
     {
         string price;
         double cafe;
+        MainMenu mymenu;
+        MenuItem reset, exit, menu, next;
+
+        ContextMenuStrip contextmenu;
+
+        int i;
 
         public Form1()
         {
@@ -27,8 +34,49 @@ namespace Volkov_HW_WinForms_5
             textBox5.Text = "54,00";
             textBox6.Text = "33,00";
             textBox7.Text = "59,00";
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox8.Text = "0";
+            textBox9.Text = "0";
+            textBox10.Text = "0";
+            textBox11.Text = "0";
+            i = 0;
             price = string.Empty;
             cafe = 0;
+
+            mymenu = new MainMenu();
+
+            menu = new MenuItem("Menu");
+            menu.Select += new EventHandler(SelectMenu1);
+            mymenu.MenuItems.Add(menu);
+
+            next = new MenuItem("Next Client");
+            next.Click += new EventHandler(SaveAndNext);
+            next.Select += new EventHandler(SelectMenu1);
+            menu.MenuItems.Add(next);
+
+            reset = new MenuItem("Reset");
+            reset.Select += new EventHandler(Reset);
+            mymenu.MenuItems.Add(reset);
+
+            exit = new MenuItem("Exit");
+            exit.Select += new EventHandler(Exit);
+            mymenu.MenuItems.Add(exit);
+
+            Menu = mymenu;
+
+
+
+            contextmenu = new ContextMenuStrip();
+            contextmenu.Items.Add("Next");
+            contextmenu.Items[0].Click += new EventHandler(SaveAndNext);
+            contextmenu.Items.Add("Reset");
+            contextmenu.Items[1].Click += new EventHandler(Reset);
+            contextmenu.Items.Add("Exit");
+            contextmenu.Items[2].Click += new EventHandler(Exit);
+            ContextMenuStrip = contextmenu;
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -50,6 +98,54 @@ namespace Volkov_HW_WinForms_5
                     break;
             }
         }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Reset(object sender, EventArgs e)
+        {
+            comboBox1.ResetText();
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox8.Text = "0";
+            textBox9.Text = "0";
+            textBox10.Text = "0";
+            textBox11.Text = "0";
+            label6.Text = "0";
+            label7.Text = "0";
+            label8.Text = "0";
+            cafe = 0;
+        }
+
+        private void SelectMenu1(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+
+        }
+
+        private void SaveAndNext(object sender, EventArgs e)
+        {
+            i++;
+            using(StreamWriter wr = new StreamWriter("SaveClient.txt", true))
+            {
+                wr.WriteLine("Client " + i);
+                wr.WriteLine("Type fuel -> " + comboBox1.Text);
+                wr.WriteLine("Price fuel -> " + textBox1.Text);
+                wr.WriteLine("Buy litr -> " + textBox2.Text);
+                wr.WriteLine("\n\nCafe");
+                wr.WriteLine("Hot dog -> " + textBox8.Text);
+                wr.WriteLine("Cafe -> " + textBox9.Text);
+                wr.WriteLine("Coca-Cola -> " + textBox10.Text);
+                wr.WriteLine("Sendwich -> " + textBox11.Text);
+                wr.WriteLine("\n\nTotal price -> " + label7.Text + "\n\n");
+            }
+
+            Reset(sender, e);
+        }
+
 
         private void radioButton1_Click(object sender, EventArgs e)
         {
