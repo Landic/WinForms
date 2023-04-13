442e79a57f6d9f865d289df118512972d072e252
Landic <danil.volkov.235@gmail.com>
2023-04-13 12:10:09 +0300

    Добавил дз за 13.04.2023

---
+++ Volkov_HW_WinForms_7/Volkov_HW_WinForms_7/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Volkov_HW_WinForms_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Следующее";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(238, 341);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Викторина 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton9);
+            this.groupBox3.Controls.Add(this.radioButton8);
+            this.groupBox3.Controls.Add(this.radioButton7);
+            this.groupBox3.Location = new System.Drawing.Point(7, 219);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Вопрос 3: Кто основатель Microsoft";
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(7, 65);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(82, 17);
+            this.radioButton9.TabIndex = 0;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Билл Гейтс";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(6, 42);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(109, 17);
+            this.radioButton8.TabIndex = 0;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Марк Цукенберг";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 19);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(67, 17);
+            this.radioButton7.TabIndex = 0;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Тим Кук";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(7, 113);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вопрос 2: Кто основатель Apple";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 65);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(82, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Джони Айв";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 42);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(87, 17);
+            this.radioButton5.TabIndex = 0;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Стив Джобс";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(67, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Тим Кук";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вопрос 1: 2+2";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "5";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "4";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(15, 15);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(246, 367);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 413);
+            this.progressBar1.Maximum = 9;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(238, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 441);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Volkov_HW_WinForms_7/Volkov_HW_WinForms_7/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volkov_HW_WinForms_7
+{
+    public partial class Form1 : Form
+    {
+        int score, i;
+        GroupBox groubox4, groupBox5, groupBox6, groupBox7,groupBox8,groupBox9;
+        RadioButton radio10, radio11, radio12, radio13, radio14, radio15, radio16, radio17, radio18;
+        RadioButton radio19, radio20, radio21, radio22, radio23, radio24, radio25, radio26, radio27;
+        TabPage page;
+
+        public Form1()
+        {
+            InitializeComponent();
+            score = 0;
+            i = 1;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 9;
+
+            radio10= new RadioButton();
+            radio10.Text = "8";
+            radio11= new RadioButton();
+            radio11.Text = "6";
+            radio12 = new RadioButton();
+            radio12.Text = "10";
+            radio13 = new RadioButton();
+            radio13.Text = "05.10.2021";
+            radio14 = new RadioButton();
+            radio14.Text = "05.09.2021";
+            radio15 = new RadioButton();
+            radio15.Text = "10.11.2020";
+            radio16 = new RadioButton();
+            radio16.Text = "14.03.2010";
+            radio17 = new RadioButton();
+            radio17.Text = "14.03.2002";
+            radio18 = new RadioButton();
+            radio18.Text = "14.03.2005";
+
+
+            radio19 = new RadioButton();
+            radio19.Text = "Харьков";
+            radio20 = new RadioButton();
+            radio20.Text = "Киев";
+            radio21 = new RadioButton();
+            radio21.Text = "Одесса";
+            radio22 = new RadioButton();
+            radio22.Text = "01.09.1960";
+            radio23 = new RadioButton();
+            radio23.Text = "29.07.1958";
+            radio24 = new RadioButton();
+            radio24.Text = "02.12.1991";
+            radio25 = new RadioButton();
+            radio25.Text = "Человек";
+            radio26 = new RadioButton();
+            radio26.Text = "Птица";
+            radio27 = new RadioButton();
+            radio27.Text = "Насекомое";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                score++;
+                radioButton1.BackColor = Color.Green;
+                groupBox1.Enabled= false;
+            }
+            if (radioButton5.Checked)
+            {
+                score++;
+                radioButton5.BackColor = Color.Green;
+                groupBox2.Enabled = false;
+            }
+            if (radioButton9.Checked)
+            {
+                score++;
+                radioButton9.BackColor = Color.Green;
+                groupBox3.Enabled = false;
+            }
+            if (radio10.Checked)
+            {
+                score++;
+                radio10.BackColor = Color.Green;
+                groubox4.Enabled = false;
+            }
+            if (radio15.Checked)
+            {
+                score++;
+                radio15.BackColor = Color.Green;
+                groupBox5.Enabled = false;
+            }
+            if (radio17.Checked)
+            {
+                score ++;
+                radio17.BackColor = Color.Green;
+                groupBox6.Enabled = false;
+            }
+            if (radio20.Checked)
+            {
+                score++;
+                radio20.BackColor = Color.Green;
+                groupBox7.Enabled = false;
+            }
+            if (radio23.Checked)
+            {
+                score++;
+                radio23.BackColor = Color.Green;
+                groupBox8.Enabled = false;
+            }
+            if (radio25.Checked)
+            {
+                score++;
+                radio25.BackColor = Color.Green;
+                groupBox9.Enabled = false;
+            }
+            progressBar1.Value = score;
+            MessageBox.Show("Вы набрали " + score + "/9", "Викторина");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+            if (i == 2)
+            {
+                TabPage page = new TabPage("Викторина " + i);
+                tabControl1.Controls.Add(page);
+                groubox4 = new GroupBox();
+                groubox4.Size = new System.Drawing.Size(205, 100);
+                groubox4.Location = new System.Drawing.Point(7, 7);
+                groubox4.Text = "Вопрос 4: 2 + 2 * 2";
+                radio10.Size = new System.Drawing.Size(31, 17);
+                radio10.Location = new System.Drawing.Point(7, 20);
+                radio11.Size = new System.Drawing.Size(31,17);
+                radio11.Location = new System.Drawing.Point(6, 43);
+                radio12.Size = new System.Drawing.Size(31, 17);
+                radio12.Location = new System.Drawing.Point(6, 66);
+                groubox4.Controls.Add(radio10);
+                groubox4.Controls.Add(radio11);
+                groubox4.Controls.Add(radio12);
+                page.Controls.Add(groubox4);
+                groupBox5= new GroupBox();
+                groupBox5.Size = new System.Drawing.Size(205, 100);
+                groupBox5.Location = new System.Drawing.Point(7, 113);
+                groupBox5.Text = "Вопрос 5: Когда вышла Windows 11";
+                radio13.Size = new System.Drawing.Size(109, 17);
+                radio13.Location = new System.Drawing.Point(6, 19);
+                radio14.Size = new System.Drawing.Size(109, 17);
+                radio14.Location = new System.Drawing.Point(6, 42);
+                radio15.Size = new System.Drawing.Size(109, 17);
+                radio15.Location = new System.Drawing.Point(6, 65);
+                groupBox5.Controls.Add(radio13);
+                groupBox5.Controls.Add(radio14);
+                groupBox5.Controls.Add(radio15);
+                page.Controls.Add(groupBox5);
+                groupBox6 = new GroupBox();
+                groupBox6.Size = new System.Drawing.Size(205, 100);
+                groupBox6.Location = new System.Drawing.Point(7, 219);
+                groupBox6.Text = "Вопрос 6: Когда основали SpaceX";
+                radio16.Size = new System.Drawing.Size(109, 17);
+                radio16.Location = new System.Drawing.Point(6, 19);
+                radio17.Size = new System.Drawing.Size(109, 17);
+                radio17.Location = new System.Drawing.Point(6, 42);
+                radio18.Size = new System.Drawing.Size(109, 17);
+                radio18.Location = new System.Drawing.Point(6, 65);
+                groupBox6.Controls.Add(radio16);
+                groupBox6.Controls.Add(radio17);
+                groupBox6.Controls.Add(radio18);
+                page.Controls.Add(groupBox6);
+            }
+            else if(i == 3)
+            {
+                TabPage page = new TabPage("Викторина " + i);
+                tabControl1.Controls.Add(page);
+                groupBox7 = new GroupBox();
+                groupBox7.Size = new System.Drawing.Size(205, 100);
+                groupBox7.Location = new System.Drawing.Point(7, 7);
+                groupBox7.Text = "Вопрос 7: Столица Украины";
+                radio19.Size = new System.Drawing.Size(109, 17);
+                radio19.Location = new System.Drawing.Point(7, 20);
+                radio20.Size = new System.Drawing.Size(109, 17);
+                radio20.Location = new System.Drawing.Point(6, 43);
+                radio21.Size = new System.Drawing.Size(109, 17);
+                radio21.Location = new System.Drawing.Point(6, 66);
+                groupBox7.Controls.Add(radio19);
+                groupBox7.Controls.Add(radio20);
+                groupBox7.Controls.Add(radio21);
+                page.Controls.Add(groupBox7);
+                groupBox8 = new GroupBox();
+                groupBox8.Size = new System.Drawing.Size(205, 100);
+                groupBox8.Location = new System.Drawing.Point(7, 113);
+                groupBox8.Text = "Вопрос 8: Когда основали NASA";
+                radio22.Size = new System.Drawing.Size(109, 17);
+                radio22.Location = new System.Drawing.Point(6, 19);
+                radio23.Size = new System.Drawing.Size(109, 17);
+                radio23.Location = new System.Drawing.Point(6, 42);
+                radio24.Size = new System.Drawing.Size(109, 17);
+                radio24.Location = new System.Drawing.Point(6, 65);
+                groupBox8.Controls.Add(radio22);
+                groupBox8.Controls.Add(radio23);
+                groupBox8.Controls.Add(radio24);
+                page.Controls.Add(groupBox8);
+                groupBox9 = new GroupBox();
+                groupBox9.Size = new System.Drawing.Size(205, 100);
+                groupBox9.Location = new System.Drawing.Point(7, 219);
+                groupBox9.Text = "Вопрос 9: Кто ты";
+                radio25.Size = new System.Drawing.Size(109, 17);
+                radio25.Location = new System.Drawing.Point(6, 19);
+                radio26.Size = new System.Drawing.Size(109, 17);
+                radio26.Location = new System.Drawing.Point(6, 42);
+                radio27.Size = new System.Drawing.Size(109, 17);
+                radio27.Location = new System.Drawing.Point(6, 65);
+                groupBox9.Controls.Add(radio25);
+                groupBox9.Controls.Add(radio26);
+                groupBox9.Controls.Add(radio27);
+                page.Controls.Add(groupBox9);
+            }
+        }
+    }
+}
