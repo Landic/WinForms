fe40d4e1db77fafd40eada200be78e49c4aed0dd
Landic <danil.volkov.235@gmail.com>
2023-04-18 11:18:57 +0300

    Добавил лабу за 18.04.2023

---
+++ Volkov_HW_WinForms_10/Volkov_HW_WinForms_10/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Volkov_HW_WinForms_10
+{
+    internal class Program
+    {
+        //static void Output2(object list)
+        //{
+        //    if (list is List<int> list2)
+        //    {
+        //        foreach (var i in list2)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    }
+        //}
+
+        internal class Bank
+        {
+            int money;
+            string name;
+            int percent;
+            Thread thread;
+
+            public Bank(string name)
+            {
+                money = 0;
+                this.name = name;
+                percent = 0;
+            }
+
+            public void Input()
+            {
+                Console.WriteLine("Enter money");
+                money = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter percent");
+                percent= int.Parse(Console.ReadLine());
+                thread = new Thread(new ThreadStart(Save));
+                thread.IsBackground = true;
+                thread.Name = "save";
+                thread.Start();
+            }
+
+            public int Money
+            {
+                get { return money; }
+                set 
+                { 
+                    money = value;
+                    thread = new Thread(new ThreadStart(Save));
+                    thread.IsBackground = true;
+                    thread.Name = "save";
+                    thread.Start();
+                }
+            }
+
+            public int Percent
+            {
+                get { return percent; }
+                set
+                {
+                    percent = value;
+                    thread = new Thread(new ThreadStart(Save));
+                    thread.IsBackground = true;
+                    thread.Name = "save";
+                    thread.Start();
+                }
+            }
+
+            public void Save()
+            {
+                using(StreamWriter writer = new StreamWriter("../../Save.txt", true))
+                {
+                    writer.WriteLine("Name -> " + name);
+                    writer.WriteLine("Money -> " + money);
+                    writer.WriteLine("Percent -> " + percent);
+                }
+                thread.Abort();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // TASK 1 //
+
+            //List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Thread thread = new Thread(new ParameterizedThreadStart(Output2));
+            //thread.IsBackground = true;
+            //thread.Name = "Output collection";
+            //thread.Start(list);
+
+            // TASK 2 //
+
+            Bank obj = new Bank("MonoBank");
+            obj.Input();
+            obj.Input();
+            obj.Percent = 10;
+        }
+    }
+}
---
+++ Volkov_HW_WinForms_9/Volkov_HW_WinForms_9/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volkov_HW_WinForms_9
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        ImageList image_list1;
+        ImageList image_list2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // инициализация treeView
+            treeView1.Nodes.Add(new TreeNode("My Computer", 0, 0));
+            foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
+                if (di.IsReady)
+                {
+                    TreeNode driveNode = new TreeNode(di.Name, 1, 1);
+                    driveNode.Tag = di.RootDirectory;
+                    treeView1.Nodes[0].Nodes.Add(driveNode);
+                    driveNode.Nodes.Add(new TreeNode());
+                }
+            }
+            image_list1 = new ImageList();
+            image_list2 = new ImageList();
+            image_list1.ColorDepth = ColorDepth.Depth32Bit;
+            image_list1.ImageSize = new Size(32, 32);
+            listView1.SmallImageList = image_list1;
+            image_list2.ColorDepth = ColorDepth.Depth32Bit;
+            image_list2.ImageSize = new Size(16, 16);
+            listView1.LargeImageList = image_list1;
+            listView1.SmallImageList = image_list2;
+
+            path = "C:\\";
+            DirectoryInfo di2 = new DirectoryInfo(path);
+            Icon icon = new Icon("icon.ico");                
+            image_list1.Images.Add(icon);
+            image_list2.Images.Add(icon);
+            foreach (DirectoryInfo dir in di2.GetDirectories())
+            {
+
+                ListViewItem lvi = new ListViewItem(dir.Name, 0);
+                lvi.Tag = dir;
+                listView1.Items.Add(lvi);
+            }
+            foreach (FileInfo file in di2.GetFiles())
+            {
+                icon = Icon.ExtractAssociatedIcon(file.FullName);
+                image_list1.Images.Add(icon);
+                image_list2.Images.Add(icon);
+                ListViewItem lvi = new ListViewItem(file.Name, image_list1.Images.Count - 1);
+                lvi.Tag = file;
+                listView1.Items.Add(lvi);
+            }
+        }
+
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
