675779c5baa6b487b63b43a15b085c7d062dcd32
Landic <danil.volkov.235@gmail.com>
2023-04-20 11:29:49 +0300

    Добавил дз за 20.04.2023

---
+++ Volkov_HW_WinForms_14/Volkov_HW_WinForms_14/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Volkov_HW_WinForms_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.снятьЗадачуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.обновитьСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(441, 415);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.снятьЗадачуToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.обновитьСписокToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 54);
+            // 
+            // снятьЗадачуToolStripMenuItem
+            // 
+            this.снятьЗадачуToolStripMenuItem.Name = "снятьЗадачуToolStripMenuItem";
+            this.снятьЗадачуToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.снятьЗадачуToolStripMenuItem.Text = "Снять задачу";
+            this.снятьЗадачуToolStripMenuItem.Click += new System.EventHandler(this.DeleteToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // обновитьСписокToolStripMenuItem
+            // 
+            this.обновитьСписокToolStripMenuItem.Name = "обновитьСписокToolStripMenuItem";
+            this.обновитьСписокToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.обновитьСписокToolStripMenuItem.Text = "Обновить список";
+            this.обновитьСписокToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Снять задачу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem снятьЗадачуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem обновитьСписокToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Volkov_HW_WinForms_14/Volkov_HW_WinForms_14/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Volkov_HW_WinForms_14
+{
+    public partial class Form1 : Form
+    {
+        Process[] processes;
+        public Form1()
+        {
+            InitializeComponent();
+            processes = Process.GetProcesses();
+            listView1.Columns.Add("Имя процесса", 210);
+            listView1.Columns.Add("ID процессора", 210);
+            foreach (var i in processes)
+            {
+                listView1.Items.Add(new ListViewItem() { Text = i.ProcessName, SubItems = { i.Id.ToString() } });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                processes[selectedItem.Index].Kill();
+            }
+        }
+
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                processes[selectedItem.Index].Kill();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            processes = null;
+            processes = Process.GetProcesses();
+            listView1.Items.Clear();
+            foreach (var i in processes)
+            {
+                listView1.Items.Add(new ListViewItem() { Text = i.ProcessName, SubItems = { i.Id.ToString() } });
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != string.Empty)
+            {
+                processes[processes.Length - 1].StartInfo.FileName = textBox1.Text;
+                processes[processes.Length - 1].Start();
+                listView1.Items.Clear();
+                foreach (var i in processes)
+                {
+                    listView1.Items.Add(new ListViewItem() { Text = i.ProcessName, SubItems = { i.Id.ToString() } });
+                }
+            }
+        }
+    }
+}
