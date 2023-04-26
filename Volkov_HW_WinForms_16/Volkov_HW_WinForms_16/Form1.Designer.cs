4c2c997efd560ddef04069be0c55ab154b9d373f
Landic <danil.volkov.235@gmail.com>
2023-04-26 20:08:01 +0300

    Добавил дз за 24.04.2023

--- Volkov_HW_WinForms_16/Volkov_HW_WinForms_16/Form1.Designer.cs
+++ Volkov_HW_WinForms_16/Volkov_HW_WinForms_16/Form1.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DirectoryBox = new System.Windows.Forms.TextBox();
+            this.WordBox = new System.Windows.Forms.TextBox();
+            this.SearchBut = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DirectoryBox
+            // 
+            this.DirectoryBox.Location = new System.Drawing.Point(12, 21);
+            this.DirectoryBox.Name = "DirectoryBox";
+            this.DirectoryBox.Size = new System.Drawing.Size(152, 20);
+            this.DirectoryBox.TabIndex = 0;
+            // 
+            // WordBox
+            // 
+            this.WordBox.Location = new System.Drawing.Point(203, 21);
+            this.WordBox.Name = "WordBox";
+            this.WordBox.Size = new System.Drawing.Size(152, 20);
+            this.WordBox.TabIndex = 0;
+            // 
+            // SearchBut
+            // 
+            this.SearchBut.Location = new System.Drawing.Point(385, 18);
+            this.SearchBut.Name = "SearchBut";
+            this.SearchBut.Size = new System.Drawing.Size(75, 23);
+            this.SearchBut.TabIndex = 1;
+            this.SearchBut.Text = "Поиск";
+            this.SearchBut.UseVisualStyleBackColor = true;
+            this.SearchBut.Click += new System.EventHandler(this.SearchBut_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 378);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SearchBut);
+            this.Controls.Add(this.WordBox);
+            this.Controls.Add(this.DirectoryBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox DirectoryBox;
+        private System.Windows.Forms.TextBox WordBox;
+        private System.Windows.Forms.Button SearchBut;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Volkov_HW_WinForms_16/Volkov_HW_WinForms_16/Form1.cs
+++ Volkov_HW_WinForms_16/Volkov_HW_WinForms_16/Form1.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +16,53 @@ namespace Volkov_HW_WinForms_16
 {
     public partial class Form1 : Form
     {
+        int count;
+        Mutex mutex;
+
         public Form1()
         {
             InitializeComponent();
+            count = 0;
+            mutex = new Mutex(false,"Search");
         }
+
+        private async void SearchBut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mutex.WaitOne();
+                textBox1.Clear();
+                count = 0;
+                DirectoryInfo dir = new DirectoryInfo(DirectoryBox.Text);
+                FileInfo[] files = await Task.Run(() => dir.GetFiles("*.txt"));
+                foreach (var i in files)
+                {
+                    using (StreamReader read = new StreamReader(i.FullName))
+                    {
+                        while(!read.EndOfStream)
+                        {
+                            string[] temp = read.ReadToEnd().Split(' ');
+                            foreach(string j in temp)
+                            {
+                                if (j.Contains(WordBox.Text))
+                                {
+                                    count++;
+                                }
+                            }
+                            
+                        }
+                    }
+                    if(count > 0)
+                    {
+                        textBox1.Text = i.Name + "\t" + i.FullName + "\t" + count;
+                    }
+                }
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+
     }
 }
